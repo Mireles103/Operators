50b77bc71e836b5aa723bcdb4d7dcfbf28744178
Jose Mireles <Mirelesjose379@gmail.com>
2023-01-24 22:45:23 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val a = 20
+    val b = 5
+
+
+    val ans1 = a + b
+    println("$a + $b = $ans1")
+    val ans2 = a - b
+    println("$a-$b = $ans2")
+    val ans3 = a * b
+    println("$a * $b = $ans3")
+    val ans4 = a / b
+    println("$a / $b = $ans4")
+    val ans5 = a % b
+    println("$a % $b = $ans5")
+}
